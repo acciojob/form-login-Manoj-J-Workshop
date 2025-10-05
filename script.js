d73ac6a4ee26f42ee2cad558f0bccf3ca3625467
Manoj-J-Workshop <manojj91298@gmail.com>
2025-10-05 10:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function getFormvalue() {
     //Write your code here
-
+	const fname = document.querySelector("input[name='fname']");
+	const lname = document.querySelector("input[name='lname']");
+	return fname.value +" " +lname.value;
 }
+
+
+
+const submit = document.querySelector('#form1 input[type="submit"]');
+
+submit.addEventListener("click", ()=>{
+	alert(getFormvalue());
+})
